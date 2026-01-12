26a69d91218d735a767eaf1b02d0cab3c53f8be4
DESKTOP-UFKBFE9\marok <maorkos987@outlook.com>
2026-01-12 11:48:30 +0100

    Adding an Auction Card component

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import AuctionCard from './AuctionCard';
 async function getDatra() {
     const res = await fetch('http://localhost:6001/search');
 
@@ -12,6 +13,10 @@ export default async function Listings() {
  
     return (
 
-    <div>{JSON.stringify(data)}</div>
+    <div>
+        {data && data.results.map((auction: any) => (
+            <AuctionCard key={auction.id} auction={auction} />
+        ))}
+    </div>
   )
 }
