202c3ca9e1eaa0127850cf9c72c31b31eb9f14ce
DESKTOP-UFKBFE9\marok <maorkos987@outlook.com>
2026-01-15 12:07:01 +0100

    Populating the session data

--- frontend/web-app/auth.ts
+++ frontend/web-app/auth.ts
@@ -3,14 +3,28 @@ import { OIDCConfig } from "next-auth/providers"
 import DuendeIDS6Provider from "next-auth/providers/duende-identity-server6" 
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    DuendeIDS6Provider({
-    id: "id-server",
-    clientId: "nextApp",
-    clientSecret: "secret",
-    issuer: "http://localhost:5000",
-    authorization: { params: { scope: "openid profile auctionApp" } },
-    idToken: true,
-  } as OIDCConfig<Profile>),
+    providers: [
+      DuendeIDS6Provider({
+      id: "id-server",
+      clientId: "nextApp",
+      clientSecret: "secret",
+      issuer: "http://localhost:5000",
+      authorization: { params: { scope: "openid profile auctionApp" } },
+      idToken: true,
+    } as OIDCConfig<Omit<Profile, "username">>),
   ],
+  callbacks:{
+    async jwt({ token, profile }) {
+      if(profile){
+        token.username = profile.username;
+      }
+      return token;
+    }
+  },
+  async session({ session, token }) {
+    if (token && session.user) {
+      session.user.username = token.username as string;
+    }
+    return session;
+  },
 })
---
+++ frontend/web-app/types/next-auth.d.ts
@@ -0,0 +1,26 @@
+import { type DefaultSession } from "next-auth"
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { type JWT } from "next-auth/jwt"
+ 
+declare module "next-auth" {
+ 
+  interface Session {
+    user: {
+      username: string
+    } & DefaultSession["user"]
+  }
+
+  interface Profile {
+    username: string;
+  }
+
+    interface User {
+      username: string;
+    }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    username: string;
+  }
+}
